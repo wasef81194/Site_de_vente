993fd85492f49b349789a3956ebd0f4c06a888bb
Wasef <alex.wasef@gmail.com>
2022-05-07 22:48:01 +0200

    install icon bootrap resolved prblm of footer

--- assets/app.js
+++ assets/app.js
@@ -5,9 +5,11 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */ 
  // loads the jquery package from node_modules
-import $ from 'jquery';
-
-     
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+require('bootstrap-icons/font/bootstrap-icons.css');
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
