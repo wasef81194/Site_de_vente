e6bef901706972572640b7dfaf376159614162a0
Wasef <alex.wasef@gmail.com>
2022-05-08 18:17:18 +0200

    disign link

--- assets/app.js
+++ assets/app.js
@@ -3,18 +3,17 @@
  *
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
- */ 
- // loads the jquery package from node_modules
-const $ = require('jquery');
+ */
+// loads the jquery package from node_modules
+const $ = require("jquery");
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
-require('bootstrap');
-require('bootstrap-icons/font/bootstrap-icons.css');
+require("bootstrap");
+require("bootstrap-icons/font/bootstrap-icons.css");
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
+import "./styles/app.scss";
 
 // start the Stimulus application
-import './bootstrap';
-import './controllers/image.js';
-import './controllers/card_produit.js';
-
+import "./bootstrap";
+import "./controllers/image.js";
+import "./controllers/card_produit.js";
