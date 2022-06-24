96b90222532b7ac741f5a30f61a345a8ceff2647
Wasef <alex.wasef@gmail.com>
2022-06-25 01:29:48 +0200

    add style page show produit

--- assets/controllers/card_produit.js
+++ assets/controllers/card_produit.js
@@ -1,13 +1,20 @@
-//fonction d'afficher les données d'une cartes 
+//fonction d'afficher les données d'une cartes
 
-// Gestion des boutons "Supprimer"
-let cards = document.querySelectorAll("[data-show-card]")
-    
-// On boucle sur links
-for(card of cards){
+$(document).ready(function () {
+  // Gestion des boutons "Supprimer"
+  let cards = document.querySelectorAll("[data-show-card]");
+
+  // On boucle sur links
+  for (card of cards) {
     // On écoute le clic
-    card.addEventListener("click", function(){
-        console.log(card,this)
-        window.location.href = this.getAttribute("href")
-    })
-}
+    card.addEventListener("click", function () {
+      console.log(card, this);
+      window.location.href = this.getAttribute("href");
+    });
+  }
+});
+
+$(window).scroll(function () {
+  var y = $(this).scrollTop();
+  console.log(y);
+});
