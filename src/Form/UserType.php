8eec7b58020fd1e8a314327fbe1a8972c041585f
Wasef <alex.wasef@gmail.com>
2022-07-04 23:45:16 +0200

    add disign page register

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,15 +6,32 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',EmailType::class,[
+                'required'=> true,
+                'constraints' => [
+                new NotBlank(["message" => "Entrer un email valid"]),
+                ]
+            ],
+            )
             //->add('roles')
-            ->add('password')
+            ->add('password', RepeatedType::class,[
+                'type' => PasswordType::class,
+                'required'=> true,
+                'label'=> 'Mot de passe',
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation du mot de passe'],
+                
+            ])
         ;
     }
 
